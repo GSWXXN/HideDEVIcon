c5ddcd321a2ade3a93595e083754da55622bc553
GSWXXN <819269088@qq.com>
2021-11-17 11:17:42 +0800

    Display the specific power value in the battery center

--- app/src/main/java/com/gswxxn/hidedevicon/main.java
+++ app/src/main/java/com/gswxxn/hidedevicon/main.java
@@ -1,5 +1,6 @@
 package com.gswxxn.hidedevicon;
 
+import android.content.Context;
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -8,77 +9,97 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import java.lang.reflect.Field;
 
 public class main implements IXposedHookLoadPackage {
-    Class<?> clazz, Build;
+    Class<?> clazz, Build, powercenter, utils;
     Field IS_DEVELOPMENT_VERSION;
     Boolean Modifiable;
 
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
-        if (!lpparam.packageName.equals("com.android.systemui")) {
-            return;
+        if (lpparam.packageName.equals("com.android.systemui")) {
+
+            clazz = XposedHelpers.findClass(
+                    "com.android.systemui.statusbar.phone.MiuiCollapsedStatusBarFragment", lpparam.classLoader);
+
+            XposedHelpers.findAndHookMethod(clazz, "showSystemIconArea", boolean.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+
+                    Build = XposedHelpers.findClass("miui.os.Build", lpparam.classLoader);
+                    IS_DEVELOPMENT_VERSION = Build.getDeclaredField("IS_DEVELOPMENT_VERSION");
+
+                    Modifiable = IS_DEVELOPMENT_VERSION.getBoolean(param.thisObject);
+
+                    if (!Modifiable) {
+                        return;
+                    }
+
+                    IS_DEVELOPMENT_VERSION.setAccessible(true);
+                    IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, false);
+                }
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+
+                    if (!Modifiable) {
+                        return;
+                    }
+
+                    IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, true);
+                    IS_DEVELOPMENT_VERSION.setAccessible(false);
+                }
+            });
+
+            XposedHelpers.findAndHookMethod(clazz, "hideSystemIconArea", boolean.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
+
+                    Build = XposedHelpers.findClass("miui.os.Build", lpparam.classLoader);
+                    IS_DEVELOPMENT_VERSION = Build.getDeclaredField("IS_DEVELOPMENT_VERSION");
+
+                    Modifiable = IS_DEVELOPMENT_VERSION.getBoolean(param.thisObject);
+
+                    if (!Modifiable) {
+                        return;
+                    }
+
+                    IS_DEVELOPMENT_VERSION.setAccessible(true);
+                    IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, false);
+                }
+
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+
+                    if (!Modifiable) {
+                        return;
+                    }
+
+                    IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, true);
+                    IS_DEVELOPMENT_VERSION.setAccessible(false);
+                }
+            });
         }
 
-        clazz = XposedHelpers.findClass(
-                "com.android.systemui.statusbar.phone.MiuiCollapsedStatusBarFragment", lpparam.classLoader);
+        if (lpparam.packageName.equals("com.miui.securitycenter")) {
+            powercenter = XposedHelpers.findClass("com.miui.powercenter.a", lpparam.classLoader);
+            utils = XposedHelpers.findClass("com.miui.powercenter.utils.q", lpparam.classLoader);
 
-        XposedHelpers.findAndHookMethod(clazz, "showSystemIconArea", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
+            XposedHelpers.findAndHookMethod(powercenter, "b", Context.class, new XC_MethodHook() {
+                @Override
+                protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                    super.beforeHookedMethod(param);
 
-                Build = XposedHelpers.findClass("miui.os.Build", lpparam.classLoader);
-                IS_DEVELOPMENT_VERSION = Build.getDeclaredField("IS_DEVELOPMENT_VERSION");
-
-                Modifiable = IS_DEVELOPMENT_VERSION.getBoolean(param.thisObject);
-                if (!Modifiable) {
-                    return;
+                    int k2 = (int) XposedHelpers.callStaticMethod(utils, "k", param.args[0]);
+                    int g2 = (int) XposedHelpers.callStaticMethod(utils, "g", param.args[0]);
+                    if (k2 == Integer.MIN_VALUE || Math.abs(g2 - k2) > 5) {
+                        k2 = g2;
+                    }
+                    param.setResult(k2 + "℃");
                 }
-
-                IS_DEVELOPMENT_VERSION.setAccessible(true);
-                IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, false);
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-
-                if (!Modifiable) {
-                    return;
-                }
-
-                IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, true);
-                IS_DEVELOPMENT_VERSION.setAccessible(false);
-            }
-        });
-
-        XposedHelpers.findAndHookMethod(clazz, "hideSystemIconArea", boolean.class, new XC_MethodHook() {
-            @Override
-            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                super.beforeHookedMethod(param);
-
-                Build = XposedHelpers.findClass("miui.os.Build", lpparam.classLoader);
-                IS_DEVELOPMENT_VERSION = Build.getDeclaredField("IS_DEVELOPMENT_VERSION");
-
-                Modifiable = IS_DEVELOPMENT_VERSION.getBoolean(param.thisObject);
-                if (!Modifiable) {
-                    return;
-                }
-
-                IS_DEVELOPMENT_VERSION.setAccessible(true);
-                IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, false);
-            }
-
-            @Override
-            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                super.afterHookedMethod(param);
-
-                if (!Modifiable) {
-                    return;
-                }
-
-                IS_DEVELOPMENT_VERSION.setBoolean(param.thisObject, true);
-                IS_DEVELOPMENT_VERSION.setAccessible(false);
-            }
-        });
+            });
+        }
     }
 }
