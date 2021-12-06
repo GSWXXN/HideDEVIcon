e1326f09fcb02128065ba87fea7a5c1a804b1f5c
GSWXXN <819269088@qq.com>
2021-12-07 01:40:21 +0800

    Remove bold

--- app/src/main/java/com/gswxxn/hidedevicon/showbattery/BatteryMain.java
+++ app/src/main/java/com/gswxxn/hidedevicon/showbattery/BatteryMain.java
@@ -1,20 +1,17 @@
 package com.gswxxn.hidedevicon.showbattery;
 
 import android.content.Context;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class BatteryMain {
 
     public static void hook(XC_LoadPackage.LoadPackageParam lpparam) {
-        // Return Battery Value instead of description
-        Class<?> powerCenter = XposedHelpers.findClass("com.miui.powercenter.a", lpparam.classLoader);
-        Class<?> utils = XposedHelpers.findClass("com.miui.powercenter.utils.q", lpparam.classLoader);
-        XposedHelpers.findAndHookMethod(powerCenter, "b", Context.class, new DealWithMethod(utils));
-
-        // Modify View
         Class<?> powerCenterA = XposedHelpers.findClass("com.miui.powercenter.a$a", lpparam.classLoader);
-        XposedHelpers.findAndHookMethod(powerCenterA, "run",
-                new DealWithView(powerCenterA));
+        Class<?> utils = XposedHelpers.findClass("com.miui.powercenter.utils.q", lpparam.classLoader);
+        Class<?> fonts = XposedHelpers.findClass("com.miui.powercenter.utils.v", lpparam.classLoader);
+
+        XposedHelpers.findAndHookMethod(powerCenterA, "run", new DealWithView(powerCenterA, utils, fonts));
     }
 }
--- app/src/main/java/com/gswxxn/hidedevicon/showbattery/DealWithView.java
+++ app/src/main/java/com/gswxxn/hidedevicon/showbattery/DealWithView.java
@@ -1,24 +1,29 @@
 package com.gswxxn.hidedevicon.showbattery;
 
-import android.annotation.SuppressLint;
 import android.app.AndroidAppHelper;
 import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
 
 import java.lang.reflect.Field;
 
 public class DealWithView extends XC_MethodHook {
     private final Class<?> powerCenterA;
+    private final Class<?> utils;
+    private final Class<?> fonts;
 
-    public DealWithView(Class<?> powerCenterA) {
+    public DealWithView(Class<?> powerCenterA, Class<?> utils, Class<?> fonts) {
         this.powerCenterA = powerCenterA;
+        this.utils = utils;
+        this.fonts = fonts;
     }
 
     public int dp2px(Context context, float dpVal) {
@@ -29,6 +34,21 @@ public class DealWithView extends XC_MethodHook {
     public int getResourceId(Context context, String name) {
         Resources r = context.getResources();
         return r.getIdentifier(name, "id", "com.miui.securitycenter");
+    }
+
+    /**
+        Original code from
+            com.miui.powercenter.a$a.b(Context)
+     */
+    public String getTemperatureValue(Class<?> utils, Context context) {
+
+        int k2 = (int) XposedHelpers.callStaticMethod(utils, "k", context);
+        int g2 = (int) XposedHelpers.callStaticMethod(utils, "g", context);
+        if (k2 == Integer.MIN_VALUE || Math.abs(g2 - k2) > 5) {
+            k2 = g2;
+        }
+
+        return String.valueOf(k2);
     }
 
     @Override
@@ -47,18 +67,21 @@ public class DealWithView extends XC_MethodHook {
         View view = (View) field.get(param.thisObject);
 
         // Set Temperature Value Attribute
-        @SuppressLint("ResourceType") TextView valueView = view.findViewById(currentTemperatureValue);
+        TextView valueView = view.findViewById(currentTemperatureValue);
         LinearLayout.LayoutParams valueParams = (LinearLayout.LayoutParams) valueView.getLayoutParams();
         valueParams.topMargin = 0;
-        valueView.setLayoutParams(valueParams);
 
+        valueView = new TextView(context);
+        valueView.setLayoutParams(valueParams);
         valueView.setTextSize(TypedValue.COMPLEX_UNIT_DIP, (float) 36.399998);
         valueView.setPadding(0, dp2px(context, (float) -3.539978), 0, 0);
         valueView.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         valueView.setGravity(Gravity.TOP | Gravity.START);
+        valueView.setTextColor(Color.parseColor("#333333"));
+        valueView.setTypeface((Typeface) XposedHelpers.callStaticMethod(fonts, "a", context));
+        valueView.setText(getTemperatureValue(utils, context));
 
         // Set Temperature Sign Attribute
-        @SuppressLint("ResourceType") LinearLayout linearLayout = view.findViewById(tempeValueContainer);
         TextView temperatureSign = new TextView(context);
         LinearLayout.LayoutParams textParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT,
                 dp2px(context, (float) 49.099983));
@@ -68,9 +91,14 @@ public class DealWithView extends XC_MethodHook {
         temperatureSign.setTextSize(TypedValue.COMPLEX_UNIT_DIP, (float) 13.099977);
         temperatureSign.setPadding(0, dp2px(context, (float) 25.3999), 0, 0);
         temperatureSign.setText("℃");
+        temperatureSign.setTypeface((Typeface) XposedHelpers.callStaticMethod(fonts, "a"), Typeface.BOLD);
         temperatureSign.setTextAlignment(View.TEXT_ALIGNMENT_VIEW_START);
         temperatureSign.setTextColor(Color.parseColor("#333333"));
 
+        // reset tempe_value_container
+        LinearLayout linearLayout = view.findViewById(tempeValueContainer);
+        linearLayout.removeAllViews();
+        linearLayout.addView(valueView);
         linearLayout.addView(temperatureSign);
     }
 }
